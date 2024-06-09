50065b83f156ebcadf622e5b4e913c6319712af8
psycheguy <yiwenmai2005@gmail.com>
2024-06-09 23:02:32 +0200

    model done

--- nn.py
+++ nn.py
@@ -1,20 +1,11 @@
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.losses import BinaryCrossentropy
-from tensorflow.keras.optimizers import adam_v2
-from tensorflow.keras.metrics import BinaryAccuracy
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras import Sequential
-from tensorflow.keras.activations import sigmoid
+import keras
+from keras import layers
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import pandas as pd
 import matplotlib.pyplot as plt
-import keras
-print(keras.__version__)
-
+import numpy as np
 
 
 # split test, train data
@@ -22,7 +13,7 @@ df = pd.read_csv("processed_data.csv")
 attr_cols = ["PassengerId","Survived","Pclass","Age","SibSp","Parch","Fare","Sex_female","Sex_male","Embarked_C","Embarked_Q","Embarked_S"]
 X = df[attr_cols]# attributes
 y = df["Survived"] # prediction label
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, train_size=0.8)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
 
 row, col = X_train.shape
 print(X_train.shape)
@@ -38,16 +29,38 @@ y_test = tf.convert_to_tensor(y_test)
 # model implementation
 # structure
 model = keras.models.Sequential()
-model.add(Dense(7, input_dim= 12, activation="relu"))
-model.add(Dense(1,activation="sigmoid"))
+model.add(layers.Dense(10, input_dim= 12, activation="relu"))
+model.add(layers.Dense(1,activation="sigmoid"))
 
 # compile
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.fit(X_train,y_train, epochs= 10, batch_size= 20)
+history= model.fit(X_train,y_train, epochs= 28) 
+
+# evaluate model on test data
+print("Evaluate on test data:")
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print("Test loss:", test_loss)
+print("Test accuracy:", test_accuracy)
+
+
+# plot loss & accuracy
+epochs = range(1,29) 
+plt.figure(figsize=(14,5))
+plt.plot(epochs, history.history['loss'], 'r*-', label='Test Loss', color="blue")
+plt.plot(epochs, history.history['accuracy'], 'r*-', label='Test Accuracy', color="red")
+plt.title(' Training Loss and Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss and Accuracy')
+plt.legend()
+plt.savefig("loss_acc.png")
+plt.show()
 
 
 # plot confusion matrix
-cm = confusion_matrix(y_test, display_labels=["Survived", "Not survived"])
-cm_dis = ConfusionMatrixDisplay(confusion_matrix= cm, display_labels= ["Survived", "Not survived"])
-cm_dis.plot()
+y_pred = model.predict(X_test)
+y_pred_classes = (y_pred > 0.5).astype("int32")  # Convert probabilities to binary class predictions
+cm = confusion_matrix(y_test, y_pred_classes)
+cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["Not Survived", "Survived"])
+cm_display.plot()
+plt.savefig("confusion_matrix.png")
 plt.show()
